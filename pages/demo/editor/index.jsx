1cbfe0c7f7db71d51bfe332f2fdbaf952c38ef5a
Zahra Rostami <zahrarostami137878@gmail.com>
2025-10-22 10:57:38 +0330

    04-07-30-Add back button to editor

--- pages/demo/editor/index.jsx
+++ pages/demo/editor/index.jsx
@@ -1,5 +1,6 @@
 import { useRef, useEffect } from 'react';
 import Editor from '@/components/editor/Editor';
+import Link from 'next/link';
 
 function IndexPage() {
     const editorRef = useRef();
@@ -13,7 +14,12 @@ function IndexPage() {
     return (
         <div>
             <div className="flex flex-col w-full justify-center bg-gray-100 px-10 py-5">
-                <h1 className="text-2xl font-bold mb-4">Editor Demo</h1>
+                <div className="flex justify-between">
+                    <h1 className="text-2xl font-bold mb-4">Editor Demo</h1>
+                    <Link href="/" className="text-xl font-bold mb-4">
+                        Back
+                    </Link>
+                </div>
                 <Editor ref={editorRef} />
             </div>
         </div>
